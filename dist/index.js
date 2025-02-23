3e96db3886452e2986428db74da83428482f8795
HawkingHuang <math0915@gmail.com>
2025-02-24 00:09:14 +0800

    L7-L9

--- dist/index.js
+++ dist/index.js
@@ -17,3 +17,37 @@ const addTwoNumbers = (a, b) => {
     return a + b;
 };
 console.log(addTwoNumbers(3, 4));
+let title;
+let things = ['I', 'He', 'She', 87, true];
+things.push(undefined);
+console.log(things);
+let mix = ['hawking', 30, true];
+console.log(mix);
+function useCoords(x, y) {
+    return [x, y];
+}
+const [x, y] = useCoords(3, 4);
+console.log(x, y);
+let userInfo;
+userInfo = ['haawking', 30];
+console.log(userInfo);
+const citizenOne = {
+    id: 777,
+    firstName: 'Hawking',
+    lastName: 'Huang',
+    age: 30
+};
+let people = [];
+people.push(citizenOne);
+console.log(people);
+console.log(citizenOne);
+const president = {
+    title: 'president',
+    basicInfo: {
+        id: 1,
+        firstName: 'Putin',
+        lastName: 'Dude',
+        age: 60
+    }
+};
+console.log(president);
--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,59 @@ const addTwoNumbers = (a: number, b: number): number => {
   return a + b
 }
 console.log(addTwoNumbers(3, 4))
+
+let title: any
+
+let things: any[] = ['I', 'He', 'She', 87, true]
+things.push(undefined)
+console.log(things)
+
+let mix: [string, number, boolean] = ['hawking', 30, true]
+console.log(mix)
+
+function useCoords (x: number, y: number): [number, number] {
+  return [x, y]
+}
+const [x, y] = useCoords(3, 4)
+console.log(x, y)
+
+let userInfo: [username: string, age: number]
+userInfo = ['haawking', 30]
+console.log(userInfo)
+
+interface Citizen {
+  id: number,
+  firstName: string,
+  lastName: string,
+  age: number
+}
+
+const citizenOne: Citizen = {
+  id: 777,
+  firstName: 'Hawking',
+  lastName: 'Huang',
+  age: 30
+}
+
+let people: Citizen[] = []
+people.push(citizenOne)
+console.log(people)
+
+console.log(citizenOne)
+
+interface Official {
+  title: string,
+  basicInfo: Citizen
+}
+
+const president: Official = {
+  title: 'president',
+  basicInfo: {
+    id: 1,
+    firstName: 'Putin',
+    lastName: 'Dude',
+    age: 60
+  }
+}
+
+console.log(president)
